ed9a0f0f25940a56cf34dcf17ce743493258a542
Chavez Harris <codedbychavez@gmail.com>
2025-02-20 19:57:54 -0400

    Init

--- db.ts
+++ db.ts
@@ -1,1 +1,48 @@
-const fs = require("fs");
+import fs from "fs";
+
+interface JSONData {
+    [key: string]: any;
+}
+
+class JSONDatabase {
+    private filename: string;
+    private data = this.loadData();
+
+    constructor(filename: string = "data.json") {
+        this.filename = filename;
+        this.data = this.loadData();
+    }
+
+    private loadData(): JSONData {
+        try {
+            return JSON.parse(fs.readFileSync(this.filename, "utf8"));
+        } catch (error) {
+            return {}
+        }
+    }
+
+    private saveData(): void {
+        fs.writeFileSync(this.filename, JSON.stringify(this.data, null, 2));
+    }
+
+    set(key: string, value: any): void {
+        this.data[key] = value;
+        this.saveData();
+    }
+
+    get(key: string): any | null {
+        return this.data[key] || null;
+    }
+
+    find(filter: Record<string, any>): any[] {
+        return Object.values(this.data).filter((obj: any) =>
+            Object.keys((filter).every((key: any) => obj[key] === filter[key])))
+    }
+
+    delete(key: string): void {
+        delete this.data[key];
+        this.saveData();
+    }
+}
+
+export default JSONDatabase;
