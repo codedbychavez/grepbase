4c0ac3636348178f463c558e74f2043e0d25e10a
Chavez Harris <codedbychavez@gmail.com>
2025-03-09 15:37:17 -0400

    Init stuff

--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="data-table overflow-x-auto">
+    <EditModal :row="selectedRow"/>
     <table class="min-w-full table-auto bg-white shadow-lg rounded-lg">
       <thead class="bg-gray-200">
       <th v-for="(value, key) in tableData[0]" :key="key"
@@ -14,9 +15,15 @@
           }}
         </td>
         <td class="flex gap-2 px-6 py-4 text-gray-700 border-t border-gray-200">
-          <button class="bg-gray-100 hover:bg-gray-300 text-sm px-2 py-1 rounded"><Plus /></button>
-          <button class="bg-gray-100 hover:bg-gray-300 text-sm px-2 py-1 rounded"><Pencil /></button>
-          <button class="bg-gray-100 hover:bg-gray-300 text-sm px-2 py-1 rounded"><Trash /></button>
+          <button class="cursor-pointer bg-gray-100 hover:bg-gray-300 text-sm px-2 py-1 rounded">
+            <Plus/>
+          </button>
+          <button @click="handleEdit(row)" class=" cursor-pointer bg-gray-100 hover:bg-gray-300 text-sm px-2 py-1 rounded">
+            <Pencil/>
+          </button>
+          <button class="cursor-pointer bg-gray-100 hover:bg-gray-300 text-sm px-2 py-1 rounded">
+            <Trash/>
+          </button>
         </td>
       </tr>
       </tbody>
@@ -29,10 +36,18 @@
 import Trash from "@/components/Icons/Trash.vue";
 import Plus from "@/components/Icons/Plus.vue";
 import Pencil from "@/components/Icons/Pencil.vue";
+import EditModal from "@/components/EditModal.vue";
+import {ref} from "vue";
 
 defineProps<{
   tableData: Array<Record<any, any>>,
 }>()
+
+const selectedRow = ref<Record<any, any>>({});
+
+const handleEdit = (row: Record<any, any>) => {
+  selectedRow.value = row;
+}
 
 </script>
 
---
+++ frontend/src/components/EditModal.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="modal fade" id="editModal" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header"></div>
+        <div class="modal-body">
+          {{ row }}
+        </div>
+        <div class="modal-footer"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{
+  row: Record<any, any>,
+}>();
+
+</script>
