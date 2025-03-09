2dc90a10ee00783d69b51648de0b9ba598ed4438
Chavez Harris <codedbychavez@gmail.com>
2025-03-09 15:19:50 -0400

    Add files

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="bg-emerald-500">
+      <div class="container">
+      <Navigation/>
+      </div>
+    </header>
+
+  <div class="container">
+    <RouterView/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navigation from "@/components/Shared/Navigation.vue";
+</script>
+
+<style scoped>
+
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+</style>
