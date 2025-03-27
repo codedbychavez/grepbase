83b2fe19feafedc8840d67cb43c598020d1d790f
Chavez Harris <codedbychavez@gmail.com>
2025-03-26 20:08:02 -0400

    Complete face-lift

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-    <notifications position="bottom right" />
-    <header class="bg-emerald-500">
-      <div class="container">
-      <Navigation/>
-      </div>
-    </header>
+  <notifications position="bottom right" />
+  <header class="bg-white shadow-md p-4">
+    <div class="container">
+      <Navigation />
+    </div>
+  </header>
 
   <div class="container p-4">
-    <RouterView/>
+    <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
 import Navigation from "@/components/Shared/Navigation.vue";
-import {Notifications} from "@kyvg/vue3-notification";
+import { Notifications } from "@kyvg/vue3-notification";
 </script>
 
 <style scoped>
-
 .container {
   max-width: 1440px;
   margin: 0 auto;
 }
-
 </style>
