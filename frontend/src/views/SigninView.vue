77d6b3897ba5a8118c7083ff15def89c2200a8d6
Chavez Harris <codedbychavez@gmail.com>
2025-04-16 18:56:38 -0400

    Update Signin

--- frontend/src/views/SigninView.vue
+++ frontend/src/views/SigninView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex h-full items-center justify-center">
-    <Form class="w-96 border border-gray-100 shadow p-4" @submit="handleSignin">
+    <Form v-slot="{ meta }" class="w-96 border border-gray-100 shadow p-4" @submit="handleSignin">
       <h2 class="text-2xl text-center">Sign In</h2>
       <div class="form-group mt-8">
         <label for="key" class="form-label text-sm text-stone-700 block capitalize">Username</label>
@@ -20,7 +20,7 @@
         <ErrorMessage name="password" class="text-sm text-red-500" />
       </div>
       <div class="form-group mt-8 text-center">
-        <button class="px-2 py-1 bg-blue-500 w-max rounded text-gray-50 cursor-pointer" type="submit">Sign In</button>
+        <button :disabled="!meta.valid" class="px-2 py-1 bg-blue-500 w-max rounded text-gray-50 cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-300" type="submit">Sign In</button>
       </div>
       <div class="mt-8">
         New to grepbase? <RouterLink to="/signup" class="text-blue-500 underline">Sign Up</RouterLink>
@@ -66,7 +66,11 @@ function validateUsername(value: any) {
 
 function validatePassword(value: any) {
   if (!value) {
-    return 'This field is required';
+    return 'This field is required'
+  }
+
+  if (value.length < 8) {
+    return 'Password must be at least 8 characters'
   }
 
   return true;
