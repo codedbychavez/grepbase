5cefc855b65781dfa72d5495885ee713e939b562
Chavez Harris <codedbychavez@gmail.com>
2025-04-16 18:56:49 -0400

    Update Signup

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex h-full items-center justify-center">
-    <Form class="w-96 border border-gray-100 shadow p-4" @submit="handleSignup">
+    <Form v-slot="{ meta }" name="form" class="w-96 border border-gray-100 shadow p-4" @submit.prevent="handleSignup">
       <h2 class="text-2xl text-center">Sign Up</h2>
       <div class="form-group mt-8">
         <label for="key" class="form-label text-sm text-stone-700 block capitalize">Username</label>
@@ -9,14 +9,22 @@
         <ErrorMessage name="username" class="text-sm text-red-500" />
       </div>
       <div class="form-group mt-4">
-        <label for="key" class="form-label text-sm text-stone-700 block capitalize">Password</label>
+        <label for="password" class="form-label text-sm text-stone-700 block capitalize">Password</label>
         <Field name="password" v-model="formData.password" :rules="validatePassword" type="text"
           class="disabled:bg-gray-200 disabled:cursor-not-allowed form-control my-1 bg-white w-full p-2 border border-gray-200 rounded-md"
           placeholder="Enter Password" />
         <ErrorMessage name="password" class="text-sm text-red-500" />
       </div>
+      <div class="form-group mt-4">
+        <label for="confirm-password" class="form-label text-sm text-stone-700 block capitalize">Confirm
+          Password</label>
+        <Field name="confirm-password" v-model="formData.confirmPassword" :rules="validateConfirmPassword" type="text"
+          class="disabled:bg-gray-200 disabled:cursor-not-allowed form-control my-1 bg-white w-full p-2 border border-gray-200 rounded-md"
+          placeholder="Enter Password" />
+        <ErrorMessage name="confirm-password" class="text-sm text-red-500" />
+      </div>
       <div class="form-group mt-8 text-center">
-        <button class="px-2 py-1 bg-blue-500 w-max rounded text-gray-50 cursor-pointer" type="submit">Sign Up</button>
+        <button :disabled="!meta.valid" class="px-2 py-1 bg-blue-500 w-max rounded text-gray-50 cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-300" type="submit">Sign Up</button>
       </div>
       <div class="mt-8">
         Already have an account? <RouterLink to="/signin" class="text-blue-500 underline">Sign In</RouterLink>
@@ -31,14 +39,19 @@ import { reactive } from 'vue';
 import { useAuthStore } from '@/stores/authStore';
 import { useRouter } from 'vue-router';
 import { notify } from '@kyvg/vue3-notification';
-import { Form, Field, ErrorMessage } from 'vee-validate';
+import { Form, Field, ErrorMessage, useForm } from 'vee-validate';
 
 const router = useRouter();
 const authStore = useAuthStore();
 
+const form = reactive(useForm({
+  name: 'form'
+}))
+
 const formData = reactive({
   username: "",
   password: "",
+  confirmPassword: ""
 })
 
 function validateUsername(value: any) {
@@ -50,8 +63,30 @@ function validateUsername(value: any) {
 }
 
 function validatePassword(value: any) {
+
   if (!value) {
-    return 'This field is required';
+    return 'This field is required'
+  }
+
+  if (value.length < 8) {
+    return 'Password must be at least 8 characters'
+  }
+
+  return true;
+}
+
+function validateConfirmPassword(value: any) {
+
+  if (!value) {
+    return 'This field is required'
+  }
+
+  if (value.length < 8) {
+    return 'Password must be at least 8 characters'
+  }
+
+  if (value !== formData.password) {
+    return 'Passwords do not match'
   }
 
   return true;
