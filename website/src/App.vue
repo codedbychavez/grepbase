7badd99a9ac5388cae6e69120e3d7ea5481d91a8
Chavez Harris <codedbychavez@gmail.com>
2025-05-04 10:54:50 -0400

    Init website

---
+++ website/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="hero h-2/4 bg-emerald-500 flex items-center justify-center p-8">
+
+    <div class="hero-content">
+      <h1 class="text-5xl text-white text-center font-black">GrepBase</h1>
+      <p class="text-gray-100 max-w-md mt-4 text-lg text-center">
+        GrepBase is a simple JSON file database with an express backend and a Vue.js frontend. It is handy for adding dummy data to test your apps during development. 
+      </p>
+    </div>
+
+  </header>
+  <main class="p-8 text-2xl font-bold text-center">
+    <h2>How it works</h2>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
